7d980592e792961158090b82f8f1b0701b3cb4fc
malcos <malcos@dp118-110.jaist.ac.jp>
2023-04-15 18:56:25 +0900

    make comment

--- sub.go
+++ sub.go
@@ -10,36 +10,38 @@ import (
 )
 
 func main() {
-
+	// channelの作成
 	msgCh := make(chan mqtt.Message)
-
-	// importしたmqttのstructで定義されたMessageHandlerに関数を渡す
+	// messageをchannelに送信する関数の作成
 	var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-		// msgChに対してmsgを送信
 		msgCh <- msg
 	}
-
+	// optsにClientOptionsインスタンスのpointerを格納
 	opts := mqtt.NewClientOptions()
-
+	//　BrokerServerのlistに追加
 	opts.AddBroker("tcp://localhost:1883")
-
+	// clientクラスのインスタンスを作成
 	c := mqtt.NewClient(opts)
-
+	// BrokerへのconnectionにErrorがないか判定
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		log.Fatalf("Mqtt error: %s", token.Error())
 	}
-
+	// subscribeするtopicが、正しく設定されているか判定
 	if subscribeToken := c.Subscribe("go-mqtt/sample,", 0, f); subscribeToken.Wait() && subscribeToken.Error() != nil {
 		log.Fatal(subscribeToken.Error())
 	}
-
+	// systemcallを受け取るchanenlの作成
 	signalCh := make(chan os.Signal, 1)
+	// systemcallがあると知らせる
 	signal.Notify(signalCh, os.Interrupt)
 
+	// forever
 	for {
 		select {
+		// メッセージをchanellから受信
 		case m := <-msgCh:
 			fmt.Printf("topic: %v, payload: %v\n", m.Topic(), string(m.Payload()))
+		// systemcallがあると知らせる
 		case <-signalCh:
 			fmt.Printf("Interrupt detected.\n")
 			c.Disconnect(1000)
